1fef2f00282ee29ba55c0b8185eddc6f430be2f4
Makoto <132969568+nx2141@users.noreply.github.com>
2025-03-16 23:08:35 +0900

    next.config.ts を更新

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // すべての API エンドポイントに適用
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://next-server-component-test.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, OPTIONS, POST",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
