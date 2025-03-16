6253e759ad74f8458b2cd10f8d18f6581aecb38e
Makoto <132969568+nx2141@users.noreply.github.com>
2025-03-16 19:52:52 +0900

    refactor:test apiからoptons削除

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,23 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function OPTIONS() {
-  return new NextResponse(null, {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-  });
-}
-
 export async function GET() {
   console.log("API /api/test was called");
-  return new NextResponse(JSON.stringify({ message: "Hello from API!" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  return NextResponse.json({ message: "Hello from Next.js!" });
 }
